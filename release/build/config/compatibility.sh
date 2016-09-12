3045dfc1f512297ed1ee9ff1aea30208d39d6fc5
Tk-Glitch <Ti3noU@gmail.com>
2016-09-12 15:39:17 +0000

    Release : Changed ramdisk address, changed the way the fstab file is edited to be more robust and compatible, restore the fstab backup each time it's edited to keep it clean. Comment'd out alternative hw binary script as they aren't used anymore.

--- release/build/config/compatibility.sh
+++ release/build/config/compatibility.sh
@@ -8,12 +8,12 @@ fi
 if [ -e /system/bin/thermald ] ; then
 	busybox mv /system/bin/thermald /system/bin/thermald_bck
 fi
-if [ ! -e /system/lib/hw/power.msm8960.so_bck ] ; then
-busybox mv /system/lib/hw/power.msm8960.so /system/lib/hw/power.msm8960.so_bck
-fi
-if [ ! -e /system/lib/hw/power.flo.so_bck ] ; then
-busybox mv /system/lib/hw/power.flo.so /system/lib/hw/power.flo.so_bck
-fi
+#if [ ! -e /system/lib/hw/power.msm8960.so_bck ] ; then
+#busybox mv /system/lib/hw/power.msm8960.so /system/lib/hw/power.msm8960.so_bck
+#fi
+#if [ ! -e /system/lib/hw/power.flo.so_bck ] ; then
+#busybox mv /system/lib/hw/power.flo.so /system/lib/hw/power.flo.so_bck
+#fi
 
 return $?
 
